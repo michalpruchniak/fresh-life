f1d47285fb765c2e4c5eabb054a91258a767fd98
Michal Pruchniak <mpruchniak@gmail.com>
2017-03-15 11:31:41 +0100

    new scrollMe script

--- js/scroll_me.js
+++ js/scroll_me.js
@@ -1,13 +1,28 @@
-var scrollMe = (function(){
-  var scrollTo = function(element){
-    "use strict"
-  var elmnt = document.getElementById(element);
-  elmnt.scrollIntoView({behavior: 'smooth'});
-  }
-  return {
-    scrollTo: scrollTo
-  }
-})();
+var scrollMe = (function(element) {
+    var navHeight = document.querySelector("nav").offsetHeight-2,
+        start = window.pageYOffset,
+        elemPosition = document.getElementById(element).offsetTop - navHeight,
+        distance = elemPosition > start ? elemPosition - start : start - elemPosition;
+
+    if (distance < 100) {
+        scrollTo(0, elemPosition); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25),
+        leap = elemPosition > start ? start + step : start - step,
+        timer = 0;
+    if (elemPosition > start) {
+        for (i=start; i<elemPosition; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leap+")", timer * speed);
+            leap += step; if (leap > elemPosition) leap = elemPosition; timer++;
+        } return;
+    }
+    for (i=start; i>elemPosition; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leap+")", timer * speed);
+        leap -= step; if (leap < elemPosition) leap = elemPosition; timer++;
+    }
+});
 
 
 var home = document.querySelectorAll(".home");
@@ -20,29 +35,29 @@ var home = document.querySelectorAll(".home");
 
   for(i = 0; i<=1; i++){
     home[i].addEventListener("click", function(){
-      scrollMe.scrollTo("fresh-life");
+      scrollMe("fresh-life");
     });
     about[i].addEventListener("click", function(){
-      scrollMe.scrollTo("about-us");
+      scrollMe("about-us");
     });
     services[i].addEventListener("click", function(){
-      scrollMe.scrollTo("our-services");
+      scrollMe("our-services");
     });
     gallery[i].addEventListener("click", function(){
-      scrollMe.scrollTo("gallery");
+      scrollMe("gallery");
     });
     blog[i].addEventListener("click", function(){
-      scrollMe.scrollTo("blog");
+      scrollMe("blog");
     });
     contact[i].addEventListener("click", function(){
-      scrollMe.scrollTo("contact");
+      scrollMe("contact");
     });
   }
 
-document.querySelector(".fa-bars").addEventListener("click", function(){
-  this.classList.toggle("close-menu");
-});
+  document.querySelector(".fa-bars").addEventListener("click", function(){
+    this.classList.toggle("close-menu");
+  });
 
-document.querySelector("#rwd-menu ul").addEventListener("click", function(){
-  document.querySelector(".fa-bars").classList.toggle("close-menu");
-});
+  document.querySelector("#rwd-menu ul").addEventListener("click", function(){
+    document.querySelector(".fa-bars").classList.toggle("close-menu");
+  });
